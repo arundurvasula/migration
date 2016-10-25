318a2c1d017df89d454ceda12fc5907fba7bda95
Arun Durvasula <arundurvasula@addr171.net185.lifesci.ucla.edu>
2016-10-24 17:04:43 -0700

    .

--- inference_v3.py
+++ inference_v3.py
@@ -1,3 +1,4 @@
+import sys
 import msprime
 import math
 import numpy as np
@@ -7,9 +8,9 @@ import scipy.stats as st
 from migration2 import one_bin
 from migration2 import two_bin
 
-delta = 0.1
+delta = 100
 data = np.array([313.406833815]) #variance in pi between
-total_sims = 1e5
+total_sims = 1e6
 
 def one_param():
     #inference procedure for 1 parameter
@@ -26,22 +27,23 @@ def one_param():
             param0.append(mig)
         num_sims = num_sims + 1
         if num_sims % 1000 == 0:
-            print("Finished ", num_sims, " simulations")
+            print("Finished", num_sims, "simulations", file=sys.stderr)
     
-    print("Number of accepted simulations: ", len(param0))
-    print("Probability of model: ", len(param0)/total_sims)
-    print("Median estimate of param0: ", np.median(param0))
-    print("95% confidence interval: ", st.t.interval(0.95, len(param0)-1, loc=np.mean(param0), scale=st.sem(param0)))
+    print("Number of accepted simulations:", len(param0))
+    print("Probability of model:", len(param0)/total_sims)
+    print("Median estimate of param0:", np.median(param0))
+    print("95% confidence interval:", st.t.interval(0.95, len(param0)-1, loc=np.mean(param0), scale=st.sem(param0)))
+    prob = len(param0)/total_sims
+    aic = 2 - 2*np.log(prob)
+    print("AIC:", aic)
     fig, ax = plt.subplots( nrows=1, ncols=1 )  # create figure & 1 axis
     plt.hist(param0)
-    plt.xlabel('Parameter estimate (m_0)')
+    plt.xlabel('Parameter estimate ')
     fig.savefig("1param.png")   # save the figure to file
     plt.close(fig)    # close the figure
-    
-    
 
 def two_params():
-    #inference procedure for 1 parameter
+    #inference procedure for 2 parameters
     # uses a uniform prior. Fagundes et al use a log-uniform prior for migration rates though
     param0 = []
     param1 = []
@@ -58,14 +60,17 @@ def two_params():
             param1.append(mig1)
         num_sims = num_sims + 1
         if num_sims % 1000 == 0:
-            print("Finished ", num_sims, " simulations")
+            print("Finished", num_sims, "simulations", file=sys.stderr)
     
-    print("Number of accepted simulations: ", len(param0))
-    print("Probability of model: ", len(param0)/total_sims)
-    print("Median estimate of param0: ", np.median(param0))
-    print("95% HPD: ", st.t.interval(0.95, len(param0)-1, loc=np.mean(param0), scale=st.sem(param0)))
-    print("Median estimate of param1: ", np.median(param1))
-    print("95% HPD: ", st.t.interval(0.95, len(param1)-1, loc=np.mean(param1), scale=st.sem(param1)))
+    print("Number of accepted simulations:", len(param0))
+    print("Probability of model:", len(param0)/total_sims)
+    print("Median estimate of param0:", np.median(param0))
+    print("95% HPD:", st.t.interval(0.95, len(param0)-1, loc=np.mean(param0), scale=st.sem(param0)))
+    print("Median estimate of param1:", np.median(param1))
+    print("95% HPD:", st.t.interval(0.95, len(param1)-1, loc=np.mean(param1), scale=st.sem(param1)))
+    prob = len(param0)/total_sims
+    aic = 2 - 2*np.log(prob)
+    print("AIC:", aic)
     fig, ax = plt.subplots( nrows=1, ncols=1 )  # create figure & 1 axis
     plt.hist(param0)
     plt.hist(param1)
